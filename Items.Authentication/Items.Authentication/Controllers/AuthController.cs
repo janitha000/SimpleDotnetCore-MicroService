745b6db6e74d04b273ce7eb6e89c63c7122f8dcb
janitha000 <JanithaT@99x.lk>
2019-04-11 11:18:35 +0530

    Added authentication service

---
+++ Items.Authentication/Items.Authentication/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Items.Authentication.Entities;
+using Items.Authentication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Items.Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthenticationService _authService;
+        private readonly ILogger logger;
+
+        public AuthController(IAuthenticationService service, ILogger<AuthController> logger)
+        {
+            this._authService = service;
+            this.logger = logger;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<string>> Register([FromBody] User user)
+        {
+            try
+            {
+                logger.LogDebug($"Called register endpoint with parameters {user.Name}, {user.Email}");
+                ApiResult registered = await this._authService.RegisterAsync(user);
+                if (registered.Status)
+                {
+                    //this._userRepository.Add(user);
+                    return Ok("User Created");
+                }
+                else
+                {
+                    logger.LogWarning("User not registered");
+                    return BadRequest(registered.Message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error when registering");
+                return Ok(new ApiResult(false, ex));
+            }
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApiResult>> Signin([FromBody] User user)
+        {
+            try
+            {
+                logger.LogDebug($"Called to login using parameters: {user.Name}");
+                ApiResult result = await this._authService.SigninAsync(user);
+                if (result.Status)
+                    return Ok(result.Message);
+                else
+                    return BadRequest(result.Message);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error when signin");
+                return Ok(new ApiResult(false, ex));
+            }
+
+        }
+    }
+}
---
+++ Items.Authentication/Items.Authentication/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Items.Authentication.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Items.Authentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer").AddJwtBearer(options =>
+            {
+                options.Audience = "e63r39b1umh8n55p5qhhn72t8";
+                options.Authority = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_fEiQUmfRQ";
+            });
+
+            services.AddScoped<IAuthenticationService, CognitoAuthenticationService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
